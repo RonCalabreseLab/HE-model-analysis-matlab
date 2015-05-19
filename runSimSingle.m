d2e5d08e068821ce1d285b952abcfaae3460499f
Cengiz Gunay <cengique@users.sf.net>
2015-05-18 20:31:06 -0400

    debugging info and sanity checks

--- runSimSingle.m
+++ runSimSingle.m
@@ -18,8 +18,20 @@ function files = runSimSingle(row_db, genesis_script, props)
       cellfun(@(x)[x ' '], getParamNames(row_db), 'UniformOutput', false);
   
   % run it
-  system(['GENESIS="lgenesis" GENESIS_PAR_ROW="' ...
-          num2str(row_db(1, 1:row_db.num_params).data, precision) ...
-          '" GENESIS_PAR_NAMES="' param_names{:}...
-          '" sim_genesis.sh ' genesis_script cmd_suffix ]);
-  
+  cmdline = ...
+      ['GENESIS="lgenesis" GENESIS_PAR_ROW="' ...
+       num2str(row_db(1, 1:row_db.num_params).data, precision) ...
+       '" GENESIS_PAR_NAMES="' param_names{:}...
+       '" sim_genesis.sh ' genesis_script cmd_suffix ];
+  if isfield(props, 'quiet') && props.quiet == 0
+      disp(['Executing: ' cmdline]);
+  end
+  [status,result] = system(cmdline);
+
+  if isfield(props, 'quiet') && props.quiet == 0
+      disp(['Output: ' result])
+  end
+
+  if status ~= 0 
+      error('Failed running command.');
+  end
